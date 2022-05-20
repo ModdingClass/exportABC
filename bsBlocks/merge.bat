7d3abbfa962ae4408f751267f5dbfe22a389bc26
AA <aa@ex.com>
2022-05-20 11:44:16 +0300

    bsBlocks folder content uploaded.

---
+++ bsBlocks/merge.bat
@@ -0,0 +1,38 @@
+copy /B ^
+part0.bs_block + ^
+part1.bs_block + ^
+URQqAAA_bs.block + ^
+URQqBBB_bs.block + ^
+URQqCCC_bs.block + ^
+URQqDDD_bs.block + ^
+part2.bs_block + ^
+URQqEEE_bs.block + ^
+part3.0_0_start_MeshDataVertexData.bs_block + ^
+part3.0_1_content_MeshDataVertexData_0_uv.bs_block + ^
+part3.0_1_content_MeshDataVertexData_1_weights.bs_block + ^
+part3.0_1_content_MeshDataVertexData_2_morphs.bs_block + ^
+part3.0_2_end_MeshDataVertexData.bs_block + ^
+URQqFFF_bs.block + ^
+URQqGGG_bs.block + ^
+part3.1_0_start_MeshDataShaderArray.bs_block + ^
+part3.1_1_content_MeshDataShaderArray.bs_block + ^
+part3.1_2_end_MeshDataShaderArray.bs_block + ^
+part4_0_before_BindMatrixInverse.bs_block + ^
+part4_1_BindMatrixInverse.bs_block + ^
+part4_2_after_BindMatrixInverse.bs_block + ^
+part4_3_start_MeshDataBlendControl.bs_block + ^
+part4_4_content_MeshDataBlendControl.bs_block + ^
+part4_5_end_MeshDataBlendControl.bs_block + ^
+URQqHHH_bs.block + ^
+URQqIII_bs.block + ^
+URQqJJJ_bs.block  + ^
+part5_0_beforeBlendControls.bs_block + ^
+part5_1_stockBlendControls.bs_block + ^
+part5_2_extraBlendControls.bs_block + ^
+part5_3_afterBlendControls.bs_block + ^
+part6.bs_block ^
+body_mod.bs
+@REM copy body_mod.bs F:\Games\VX\Addons\000.Custom.Body.P741.B741.Caroline\Scenes\Shared\Body\body741.bs 
+@REM copy ..\AcBody741Collision.bs "F:\Games\VX\Addons\000.Custom.Body.P741.B741.Caroline\Scripts\Luder\Person.G=01\AcBody741Collision.bs" 
+
+exit
